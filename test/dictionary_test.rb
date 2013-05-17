b403a3ca50f5ce448b8edbf98835154e926206b9
Remo Mueller <remosm@gmail.com>
2013-05-17 14:03:49 -0400

    New spout data dictionary project layout added

---
+++ test/dictionary_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class DictionaryTest < Test::Unit::TestCase
+  include Spout::Tests
+
+  # You may add additional tests here
+  # def test_truth
+  #   assert true
+  # end
+end

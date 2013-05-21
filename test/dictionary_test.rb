735c13053745de1fb95d5731299f565771af71b5
Remo Mueller <remosm@gmail.com>
2013-05-21 13:04:55 -0400

    Added shhs domains

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class DictionaryTest < Test::Unit::TestCase
-  # include Spout::Tests
+  include Spout::Tests
 
-  include Spout::Tests::JsonValidation
-  include Spout::Tests::VariableTypeValidation
+  # include Spout::Tests::JsonValidation
+  # include Spout::Tests::VariableTypeValidation
   # include Spout::Tests::DomainExistenceValidation
-  include Spout::Tests::DomainFormat
+  # include Spout::Tests::DomainFormat
 end

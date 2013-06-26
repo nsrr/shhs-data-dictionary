eaa5e4d6f50762d172284c9258dfcc3c3e988687
Remo Mueller <remosm@gmail.com>
2013-06-26 15:34:52 -0400

    Added a test to ensure that new variables that are added are unique

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -7,4 +7,10 @@ class DictionaryTest < Test::Unit::TestCase
   # include Spout::Tests::VariableTypeValidation
   # include Spout::Tests::DomainExistenceValidation
   # include Spout::Tests::DomainFormat
+
+  def test_variable_uniqueness
+    files = Dir.glob("variables/**/*.json").collect{|file| file.split('/').last }
+    assert_equal [], files.select{ |f| files.count(f) > 1 }.uniq
+  end
+
 end

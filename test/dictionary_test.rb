96389286f8cc87cc58b12d7ea08ffa193007d2bf
Nick Testani <ntestani@partners.org>
2014-03-07 14:06:52 -0500

    Ensures that units are in the plural form, fixes #63

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -2,4 +2,21 @@ require 'test_helper'
 
 class DictionaryTest < Test::Unit::TestCase
   include Spout::Tests
+
+  VALID_UNITS = ['minute', 'days', 'drinks', 'bottles', 'glasses', 'cans', 'cups', 'cigarettes', 'years', 'naps', 'days from index date', 'percent', 'score', 'millimeters of mercury', 'cigars', 'bowls', 'day', 'hour', 'beats per minute', 'replacements', 'kohms', 'replacements', 'year', 'grade', 'cigarettes per day', 'contacts', 'pack years', 'drinks per day', 'event count', 'second', 'events per hour', 'liter', 'milligram per deciliter', 'index', 'kilograms', 'kilogram', 'centimeter', 'kilogram per square meter', 'number of events', ''  '']
+
+  def assert_units(units, msg = nil)
+    full_message = build_message(msg, "? invalid units. Valid types: #{VALID_UNITS.join(', ')}", units)
+    assert_block(full_message) do
+      VALID_UNITS.include?(units)
+    end
+  end
+
+  Dir.glob("variables/**/*.json").each do |file|
+    if ['numeric','integer'].include?(json_value(file, :type))
+      define_method("test_units: "+file) do
+        assert_units json_value(file, :units)
+      end
+    end
+  end
 end

7251dfafd99bd7d435d14ab1bdcd3ca08c2311a7
Remo Mueller <remosm@gmail.com>
2014-03-14 11:37:13 -0400

    SHHS1 Adverse Event variables now refernce the appropriate form

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -37,4 +37,27 @@ class DictionaryTest < Test::Unit::TestCase
     assert_equal [], files.select{ |f| files.count(f) > 1 }.uniq
   end
 
+  def assert_form_existence(item, msg = nil)
+    form_names = Dir.glob("forms/**/*.json").collect{|file| file.split('/').last.to_s.downcase.split('.json').first}
+
+    result = begin
+      (form_names | JSON.parse(File.read(item))["forms"]).size == form_names.size
+    rescue JSON::ParserError
+      false
+    end
+
+    full_message = build_message(msg, "One or more forms referenced by ? does not exist.", item)
+    assert_block(full_message) do
+      result
+    end
+  end
+
+  Dir.glob("variables/**/*.json").each do |file|
+    if (not [nil, ''].include?(JSON.parse(File.read(file))["forms"]) rescue false)
+      define_method("test_form_exists: "+file) do
+        assert_form_existence file
+      end
+    end
+  end
+
 end

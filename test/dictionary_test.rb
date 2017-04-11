503544ddaf2bfe9a20112b160708338b25f66969
Michael Rueschman <michael.rueschman@gmail.com>
2017-04-11 09:42:13 -0400

    Incorporate new variable labels, fix #1090

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -1,44 +1,49 @@
-require 'test_helper'
-require 'colorize'
+# frozen_string_literal: true
 
+require 'test_helper'
+
+# Launches default Spout tests and custom tests for specific to this dictionary.
 class DictionaryTest < Minitest::Test
   # This line includes all default Spout Dictionary tests
   include Spout::Tests
 
-  # This line provides access to @variables, @forms, and @domains
-  # iterators that can be used to write custom tests
+  # This line provides access to @variables, @forms, and @domains iterators
+  # that can be used to write custom tests.
   include Spout::Helpers::Iterators
 
-  # These tests can be removed when spout v0.10.0 is released as it will be included by default
-  @variables.each do |variable|
-    define_method("test_no_more_than_255_display_name: "+variable.path) do
-      assert_operator 255, :>=, variable.display_name.size
-    end
-  end
+  # Example 1: Create custom tests to show that `integer` and `numeric`
+  # variables have a valid unit type.
+  VALID_UNITS = [nil, "", "microvolts squared per hertz", "hertz", "arousals",
+    "car accidents", "miles", "beats per minute", "bottles", "bowls", "cans",
+    "centimeters", "central apnea events", "cigarettes", "cigarettes per day",
+    "cigars", "cups", "days", "days from index date", "drinks",
+    "drinks per day", "events per hour", "glasses", "hours",
+    "hypopnea events", "index", "kilograms", "kilograms per square meter",
+    "liters", "milligrams per deciliter", "millimeters",
+    "millimeters of mercury", "minutes", "naps", "number of events",
+    "obstructive apnea events", "oxygen desaturation events", "pack years",
+    "percent", "seconds", "stage shifts", "years",
+    "seconds from start of recording", "milliseconds", "milliseconds squared"]
 
-  VALID_UNITS = ["", "microvolts squared per hertz", "hertz", "arousals", "car accidents", "miles",
-    "beats per minute", "bottles", "bowls", "cans", "centimeters",
-    "central apnea events", "cigarettes", "cigarettes per day", "cigars", "cups", "days",
-    "days from index date", "drinks", "drinks per day", "events per hour", "glasses", "hours",
-    "hypopnea events", "index", "kilograms", "kilograms per square meter", "liters",
-    "milligrams per deciliter", "millimeters", "millimeters of mercury", "minutes", "naps",
-    "number of events", "obstructive apnea events", "oxygen desaturation events", "pack years",
-    "percent", "seconds", "stage shifts", "years", "seconds from start of recording",
-    "milliseconds", "milliseconds squared"]
-
-  @variables.select{|v| ['numeric','integer'].include?(v.type)}.each do |variable|
-    define_method("test_units: "+variable.path) do
-      message = "\"#{variable.units}\"".colorize( :red ) + " invalid units.\n" +
+  @variables.select { |v| %w(numeric integer).include?(v.type) }.each do |variable|
+    define_method("test_units: #{variable.path}") do
+      message = "\"#{variable.units}\"".colorize(:red) + " invalid units.\n" +
                 "             Valid types: " +
-                VALID_UNITS.sort.collect{|u| u.inspect.colorize( :white )}.join(', ')
+                VALID_UNITS.sort_by(&:to_s).collect { |u| u.inspect.colorize(:white) }.join(', ')
       assert VALID_UNITS.include?(variable.units), message
     end
   end
 
-  USED_UNITS = @variables.collect{|v| v.units}.uniq.compact
+  # Example 2: Create custom tests to show that variables have 2 or more labels.
+  # @variables.select { |v| %w(numeric integer).include?(v.type) }.each do |variable|
+  #   define_method("test_at_least_two_labels: #{variable.path}") do
+  #     assert_operator 2, :<=, variable.labels.size
+  #   end
+  # end
 
-  def test_no_unused_units
-    assert_equal [], VALID_UNITS - USED_UNITS
-  end
-
+  # Example 3: Create regular Ruby tests
+  # You may add additional tests here
+  # def test_truth
+  #   assert true
+  # end
 end
